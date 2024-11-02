ba47cb13bcc2dd737daf429eb501137a1a8c0d95
debmit97-hll <debjit.bhattacharjee@gohighlevel.com>
2024-11-02 13:40:12 +0530

    get and set added

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 const net = require("net");
 
+const store = new Map()
+
 function handlePing() {
     return '+PONG\r\n'
 }
@@ -8,13 +10,31 @@ function handleEcho(echoArg) {
     return `$${echoArg[0].length}\r\n${echoArg[0]}\r\n`
 }
 
+function handleSet(setArgs) {
+    const [key, value] = setArgs
+    store.set(key, value)
+    return '+OK\r\n'
+}
+
+function handleGet(getArg) {
+    const [key] = getArg
+    if(store.has(key)) {
+        return `$${store.get(key).length}\r\n${store.get(key)}\r\n`
+    }
+    return `$-1\r\n`
+}
+
 function commandResponse(commandString) {
     const commandArray = commandString.split(' ')
-    switch(commandArray[0].toUpperCase()) {
-        case 'PING': 
+    switch (commandArray[0].toUpperCase()) {
+        case 'PING':
             return handlePing()
         case 'ECHO':
             return handleEcho(commandArray.slice(1))
+        case 'SET':
+            return handleSet(commandArray.slice(1))
+        case 'GET':
+            return handleGet(commandArray.slice(1))
     }
 }
 
@@ -24,9 +44,9 @@ function commandParser(commandString) {
     const numArgs = parseInt(commandSplit[0].substring(1))
     let len = 0
     let string = ''
-    for(let i=0; i<numArgs; i++) {
-        len = parseInt(commandSplit[i*2+1].substring(1))
-        string = string+` ${commandSplit[i*2+2].substring(0, len)}`
+    for (let i = 0; i < numArgs; i++) {
+        len = parseInt(commandSplit[i * 2 + 1].substring(1))
+        string = string + ` ${commandSplit[i * 2 + 2].substring(0, len)}`
     }
     return string.trim()
 }
@@ -35,8 +55,8 @@ function commandParser(commandString) {
 const server = net.createServer((connection) => {
     connection.on('data', (data) => {
         const command = commandParser(data.toString())
-         connection.write(commandResponse(command))
-      });
+        connection.write(commandResponse(command))
+    });
 });
 
 server.listen(6379, "127.0.0.1");
