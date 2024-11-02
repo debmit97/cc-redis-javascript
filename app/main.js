4174e90c0dbb5e5cb23eedf819907f51edb9436f
debmit97-hll <debjit.bhattacharjee@gohighlevel.com>
2024-11-02 13:31:27 +0530

    command parser for ping and echo

--- app/main.js
+++ app/main.js
@@ -1,12 +1,41 @@
 const net = require("net");
 
+function handlePing() {
+    return '+PONG\r\n'
+}
+
+function handleEcho(echoArg) {
+    return `$${echoArg[0].length}\r\n${echoArg[0]}\r\n`
+}
+
+function commandResponse(commandString) {
+    const commandArray = commandString.split(' ')
+    switch(commandArray[0].toUpperCase()) {
+        case 'PING': 
+            return handlePing()
+        case 'ECHO':
+            return handleEcho(commandArray.slice(1))
+    }
+}
+
+
+function commandParser(commandString) {
+    const commandSplit = commandString.split('\r\n')
+    const numArgs = parseInt(commandSplit[0].substring(1))
+    let len = 0
+    let string = ''
+    for(let i=0; i<numArgs; i++) {
+        len = parseInt(commandSplit[i*2+1].substring(1))
+        string = string+` ${commandSplit[i*2+2].substring(0, len)}`
+    }
+    return string.trim()
+}
+
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
     connection.on('data', (data) => {
-        if (data.toString() === '*1\r\n$4\r\nPING\r\n') {
-          connection.write('+PONG\r\n');
-        }
-       
+        const command = commandParser(data.toString())
+         connection.write(commandResponse(command))
       });
 });
 
