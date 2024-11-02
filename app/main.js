d5e2f965759c1b3ef263c90987bd39bbe0557d77
debmit97-hll <debjit.bhattacharjee@gohighlevel.com>
2024-11-02 12:44:07 +0530

    pong on every ping

--- app/main.js
+++ app/main.js
@@ -2,11 +2,12 @@ const net = require("net");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  connection.write('+PONG\r\n')
-
-  connection.on('data', () => {
-    connection.write('+PONG\r\n')
-  })
+    connection.on('data', (data) => {
+        if (data.toString() === '*1\r\n$4\r\nPING\r\n') {
+          connection.write('+PONG\r\n');
+        }
+       
+      });
 });
 
 server.listen(6379, "127.0.0.1");
