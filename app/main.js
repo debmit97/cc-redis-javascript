bd8f3467abd79568145523d2de79504bf26f4629
debmit97-hll <debjit.bhattacharjee@gohighlevel.com>
2024-11-04 15:39:38 +0530

    read rdb files and support keys

--- app/main.js
+++ app/main.js
@@ -1,83 +1,94 @@
 const net = require("net");
+const { readRdbFile } = require("./parseRDB.js");
 
-const store = new Map()
+const store = new Map();
 
 function handlePing() {
-    return '+PONG\r\n'
+  return "+PONG\r\n";
 }
 
 function handleEcho(echoArg) {
-    return `$${echoArg[0].length}\r\n${echoArg[0]}\r\n`
+  return `$${echoArg[0].length}\r\n${echoArg[0]}\r\n`;
 }
 
 function handleSet(setArgs) {
-    const [key, value] = setArgs
-    store.set(key, value)
-    if(setArgs.length > 2) {
-        if(setArgs[2].toUpperCase() === 'PX') {
-            setTimeout(() => {
-                store.delete(key)
-            }, parseInt(setArgs[3]))
-        }
+  const [key, value] = setArgs;
+  store.set(key, value);
+  if (setArgs.length > 2) {
+    if (setArgs[2].toUpperCase() === "PX") {
+      setTimeout(() => {
+        store.delete(key);
+      }, parseInt(setArgs[3]));
     }
-    return '+OK\r\n'
+  }
+  return "+OK\r\n";
 }
 
 function handleGet(getArg) {
-    const [key] = getArg
-    if(store.has(key)) {
-        return `$${store.get(key).length}\r\n${store.get(key)}\r\n`
-    }
-    return `$-1\r\n`
+  const [key] = getArg;
+  if (store.has(key)) {
+    return `$${store.get(key).length}\r\n${store.get(key)}\r\n`;
+  }
+  return `$-1\r\n`;
 }
 
 function handleConfig(configArgs) {
-    const [command, arg] = configArgs
-    if(command.toUpperCase() === 'GET') {
-        if(arg === 'dir') {
-            return `*2\r\n$3\r\ndir\r\n$${process.argv[3].length}\r\n${process.argv[3]}\r\n`
-        } else if (arg === 'dbfilename') {
-            return `*2\r\n$10\r\ndbfilename\r\n$${process.argv[5].length}\r\n${process.argv[5]}\r\n`
-        }
+  const [command, arg] = configArgs;
+  if (command.toUpperCase() === "GET") {
+    if (arg === "dir") {
+      return `*2\r\n$3\r\ndir\r\n$${process.argv[3].length}\r\n${process.argv[3]}\r\n`;
+    } else if (arg === "dbfilename") {
+      return `*2\r\n$10\r\ndbfilename\r\n$${process.argv[5].length}\r\n${process.argv[5]}\r\n`;
     }
-    
+  }
+}
+
+function handleKeys(keyArgs) {
+  const db = readRdbFile();
+  const keys = Object.keys(db);
+  let response = "";
+  for (let key of keys) {
+    response += `$${key.length}\r\n${key}\r\n`;
+  }
+  return `*${keys.length}\r\n${response}`
 }
 
 function commandResponse(commandString) {
-    const commandArray = commandString.split(' ')
-    switch (commandArray[0].toUpperCase()) {
-        case 'PING':
-            return handlePing()
-        case 'ECHO':
-            return handleEcho(commandArray.slice(1))
-        case 'SET':
-            return handleSet(commandArray.slice(1))
-        case 'GET':
-            return handleGet(commandArray.slice(1))
-        case 'CONFIG':
-            return handleConfig(commandArray.slice(1))
-    }
+  const commandArray = commandString.split(" ");
+  switch (commandArray[0].toUpperCase()) {
+    case "PING":
+      return handlePing();
+    case "ECHO":
+      return handleEcho(commandArray.slice(1));
+    case "SET":
+      return handleSet(commandArray.slice(1));
+    case "GET":
+      return handleGet(commandArray.slice(1));
+    case "CONFIG":
+      return handleConfig(commandArray.slice(1));
+    case "KEYS":
+      return handleKeys(commandArray.slice(1));
+  }
 }
-
 
 function commandParser(commandString) {
-    const commandSplit = commandString.split('\r\n')
-    const numArgs = parseInt(commandSplit[0].substring(1))
-    let len = 0
-    let string = ''
-    for (let i = 0; i < numArgs; i++) {
-        len = parseInt(commandSplit[i * 2 + 1].substring(1))
-        string = string + ` ${commandSplit[i * 2 + 2].substring(0, len)}`
-    }
-    return string.trim()
+  const commandSplit = commandString.split("\r\n");
+  const numArgs = parseInt(commandSplit[0].substring(1));
+  let len = 0;
+  let string = "";
+  for (let i = 0; i < numArgs; i++) {
+    len = parseInt(commandSplit[i * 2 + 1].substring(1));
+    string = string + ` ${commandSplit[i * 2 + 2].substring(0, len)}`;
+  }
+  return string.trim();
 }
 
-// Uncomment this block to pass the first stage
-const server = net.createServer((connection) => {
-    connection.on('data', (data) => {
-        const command = commandParser(data.toString())
-        connection.write(commandResponse(command))
-    });
-});
+// // Uncomment this block to pass the first stage
 
+const server = net.createServer((connection) => {
+  connection.on("data", (data) => {
+    const command = commandParser(data.toString());
+    connection.write(commandResponse(command));
+  });
+});
 server.listen(6379, "127.0.0.1");
---
+++ app/parseRDB.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+
+const db = {}
+
+//Reads RDB File
+const readRdbFile = () => {
+  const opCodes = {
+    resizeDb: "fb",
+  };
+  let i = 0;
+  const dirName = process.argv[3];
+  const fileName = process.argv[5];
+  const filePath = dirName + "/" + fileName;
+  console.log(`DIr: ${dirName} ,Filenamde :${fileName}`);
+  console.log(`Path`, filePath);
+  const dataBuffer = fs.readFileSync(filePath);
+  console.log("Hex data:", dataBuffer.toString("hex"));
+  const getNextNBytes = (n) => {
+    let nextNBytes = Buffer.alloc(n);
+    for (let k = 0; k < n; k++) {
+      nextNBytes[k] = dataBuffer[i];
+      i++;
+    }
+    return nextNBytes;
+  };
+  const getNextObjLength = () => {
+    const firstByte = dataBuffer[i];
+    const twoBits = firstByte >> 6;
+    let length = 0;
+    switch (twoBits) {
+      case 0b00:
+        length = firstByte ^ 0b00000000;
+        i++;
+        break;
+    }
+    return length;
+  };
+  const hashTable = () => {
+    const nextObjLength = getNextObjLength();
+    const nextNBytes = getNextNBytes(nextObjLength);
+  };
+  const expiryHashTable = () => {
+    const nextObjLength = getNextObjLength();
+    const nextNBytes = getNextNBytes(nextObjLength);
+  };
+  const resizeDb = () => {
+    console.log("Inside resizedb");
+    i++;
+    hashTable();
+    expiryHashTable();
+    const keyLength = getNextObjLength();
+    const key = getNextNBytes(keyLength);
+    const valueLength = getNextObjLength();
+    const value = getNextNBytes(valueLength);
+    console.log("Key:", key.toString(), "value:", value.toString());
+    db[key] = value;
+  };
+  while (i < dataBuffer.length) {
+    const currentHexByte = dataBuffer[i].toString(16);
+    if (currentHexByte === opCodes.resizeDb) resizeDb();
+    i++;
+  }
+  return db
+}
+
+module.exports = { readRdbFile };
