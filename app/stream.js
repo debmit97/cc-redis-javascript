73fc49407a22443e95b8db9d0db06bb5e7cd0218
debmit97-hll <debjit.bhattacharjee@gohighlevel.com>
2024-11-12 17:18:44 +0530

    codecrafters submit [skip ci]

--- app/stream.js
+++ app/stream.js
@@ -5,17 +5,18 @@ let stream = null
 class RedisStream {
     streamName = ''
     streamData = {}
-    lastEntryKey = ''
+    lastEntryKey = '0-0'
 
     validateXAdd(key, conn) {
         const keyMilliSec = parseInt(key.split('-')[0])
         const keySeq = parseInt(key.split('-')[1])
-        const lastMilliSec = parseInt(key.split('-')[0])
-        const lastSeq = parseInt(key.split('-')[1])
+        const lastMilliSec = parseInt(this.lastEntryKey.split('-')[0])
+        const lastSeq = parseInt(this.lastEntryKey.split('-')[1])
+        console.log(keyMilliSec, keySeq, lastMilliSec, lastSeq)
         if(keyMilliSec === 0 && keySeq === 0) {
             conn.write(toSimpleError('ERR The ID specified in XADD must be greater than 0-0'))
             return false
-        } else if (keyMilliSec < lastMilliSec || keyMilliSec === lastMilliSec && keySeq < lastSeq) {
+        } else if (keyMilliSec < lastMilliSec || keyMilliSec === lastMilliSec && keySeq <= lastSeq) {
             conn.write(toSimpleError('ERR The ID specified in XADD is equal or smaller than the target stream top item'))
             return false
         }
